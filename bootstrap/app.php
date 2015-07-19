861fca532f14701bdf23748ae39c76cdf041ce00
iftekher sunny <iftekhersunny@hotmail.com>
2015-07-19 21:37:18 +0600

    Added database config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,8 @@ $app = new Sun\Application([
 
 Sun\AlienLoader::load();
 
+$app->bootDatabase();
+
 $app->group(['namespace' => 'App\Controllers'], function () use ($app) {
 
     require_once __DIR__ . '/../app/routes.php';
