cf8d4929ab6ddc1effb0143b6364e5d20cd7ff4f
iftekher sunny <iftekhersunny@hotmail.com>
2015-07-19 20:40:10 +0600

    Added mail config

---
+++ config/alien.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'File' => 'Sun\FilesystemAlien',
+    'Mail' => 'SunMailer\MailerAlien',
+    'View' => 'SunMailer\ViewAlien',
+    'Flash' => 'Sun\FlashAlien',
+    'Session' => 'Sun\SessionAlien'
+
+];
